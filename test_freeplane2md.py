adc063299e9687fa1b6a7fdbdf1c26f8cef0912e
Oliver <oliver.ciupke@gmail.com>
2021-12-24 15:58:34 +0100

    Fix #3: Dos/unix line endings in automated tests

--- test_freeplane2md.py
+++ test_freeplane2md.py
@@ -8,32 +8,39 @@ import filecmp
 def test_convert_file():
     """Test file containing different freeplane features"""
     freeplane2md.convert_file('test/test.mm', 'test/test.md')
-    assert(filecmp.cmp('test/expected.md', 'test/test.md'))
+    with open('test/expected.md') as expected, open('test/test.md') as testee:
+        assert(expected.readlines() == testee.readlines())
 
 def test_headerlevel_2():
     """Testing option for different headerlevel"""
     freeplane2md.convert_file('test/test.mm', 'test/test-headerlevel2.md', headerlevel=2)
-    assert(filecmp.cmp('test/expected-headerlevel2.md', 'test/test-headerlevel2.md'))
+    with open('test/expected-headerlevel2.md') as expected, open('test/test-headerlevel2.md') as testee:
+        assert(expected.readlines() == testee.readlines())
 
 def test_headerlevel_3():
     """Testing option for different headerlevel"""
     freeplane2md.convert_file('test/test.mm', 'test/test-headerlevel3.md', headerlevel=3)
-    assert(filecmp.cmp('test/expected-headerlevel3.md', 'test/test-headerlevel3.md'))
+    with open('test/expected-headerlevel3.md') as expected, open('test/test-headerlevel3.md') as testee:
+        assert(expected.readlines() == testee.readlines())
 
 def test_headerlevel_0():
     """Testing option for different headerlevel"""
     freeplane2md.convert_file('test/test.mm', 'test/test-headerlevel0.md', headerlevel=0)
-    assert(filecmp.cmp('test/expected-headerlevel0.md', 'test/test-headerlevel0.md'))
+    with open('test/expected-headerlevel0.md') as expected, open('test/test-headerlevel0.md') as testee:
+        assert(expected.readlines() == testee.readlines())
 
 def test_icons():
     """Icons to be tested"""
     freeplane2md.convert_file('test/test-icons.mm', 'test/test-icons.md')
-    assert(filecmp.cmp('test/expected-icons.md', 'test/test-icons.md'))
+    with open('test/expected-icons.md') as expected, open('test/test-icons.md') as testee:
+        assert(expected.readlines() == testee.readlines())
 
 def test_edge_cases():
     """Several edge-cases, such as empty nodes, overlapping links and
     connectors etc.
     """
     freeplane2md.convert_file('test/test-edge-cases.mm', 'test/test-edge-cases.md', headerlevel=2)
-    assert(filecmp.cmp('test/expected-edge-cases.md', 'test/test-edge-cases.md'))
+    with open('test/expected-edge-cases.md') as expected, open('test/test-edge-cases.md') as testee:
+        assert(expected.readlines() == testee.readlines())
+
 
